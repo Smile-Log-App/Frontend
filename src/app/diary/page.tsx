5cf2679de037803468800717e789e05ce24e960e
youdame <112458620+youdame@users.noreply.github.com>
2024-10-26 15:47:28 +0900

    fix : 빌드 에러 해결

--- src/app/diary/page.tsx
+++ src/app/diary/page.tsx
@@ -1,5 +1,10 @@
 import DiaryPage from "@/app/diary/diary-page";
+import { Suspense } from "react";
 
 export default function page() {
-  return <DiaryPage />;
+  return (
+    <Suspense fallback={null}>
+      <DiaryPage />
+    </Suspense>
+  );
 }
--- src/app/tree/page.tsx
+++ src/app/tree/page.tsx
@@ -1,5 +1,10 @@
 import TreePage from "@/app/tree/tree-page";
+import { Suspense } from "react";
 
 export default function HomePage() {
-  return <TreePage />;
+  return (
+    <Suspense fallback={null}>
+      <TreePage />
+    </Suspense>
+  );
 }
